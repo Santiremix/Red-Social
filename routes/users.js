a289869b132487b828fb04833180ce5bbb2f0e15
Santiremix <santiagoguevara118@gmail.com>
2022-06-08 17:25:53 +0200

    Arreglo del ultimo endpoint

--- routes/users.js
+++ routes/users.js
@@ -8,7 +8,7 @@ router.post('/', UserController.register)
 router.get('/confirm/:emailToken', UserController.confirm);
 router.post('/login', UserController.login)
 router.delete('/logout', authentication, UserController.logout)
-router.delete('/deleteUser', authentication,isAdmin, UserController.deleteUserByAdmin)
+router.delete('/deleteUser/:id', authentication,isAdmin, UserController.deleteUserByAdmin)
 
 router.get('/info', authentication, UserController.getInfo)
 
